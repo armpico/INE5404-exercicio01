5d6b3e78c7772e57113eaae7243edab9ef2a3ba9
pixels <pixels@sunbro.localdomain>
2017-08-06 20:04:34 +0000

    Criei a cclasse TesteGit20170806

---
+++ TesteGit-2017-08-06/src/testegit/pkg2017/pkg08/pkg06/TesteGit20170806.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testegit.pkg2017.pkg08.pkg06;
+
+/**
+ *
+ * @author pixels
+ */
+public class TesteGit20170806 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
