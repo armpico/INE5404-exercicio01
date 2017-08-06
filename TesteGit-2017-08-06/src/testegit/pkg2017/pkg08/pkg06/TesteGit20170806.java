e44a1106a8d1cd0c18fb05a5de3486dad1a5182b
pixels <pixels@sunbro.localdomain>
2017-08-06 20:11:12 +0000

    Adicionei feijoada ao método main

--- TesteGit-2017-08-06/src/testegit/pkg2017/pkg08/pkg06/TesteGit20170806.java
+++ TesteGit-2017-08-06/src/testegit/pkg2017/pkg08/pkg06/TesteGit20170806.java
@@ -15,7 +15,7 @@ public class TesteGit20170806 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("NADA ACONTECE, FEIJOADA");
     }
     
 }
